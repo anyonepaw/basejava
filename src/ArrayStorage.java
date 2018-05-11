f5227f12358a7defe615a8c4e6823318e9ac93ee
anyonepaw <32063879+anyonepaw@users.noreply.github.com>
2018-05-11 21:24:11 +0300

    Fix bugs.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -27,40 +27,24 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
 
-    /**
-     * Removes all elements
-     */
     void clear() {
         storage = new Resume[10000];
         size = 0;
     }
 
-    /**
-     * Adds a new Resume
-     *
-     * @param r
-     */
+
     void save(Resume r) {
 
-        //блок необходим, чтобы в массив не был сохранен
-        // r.uuid = null (человек с пустым резюме) при команде save
-        try {
-            if (!r.uuid.equals("null")) {
-                //r1, r2, r3,..., rn, null <- add a new element here
-                storage[size] = r;
-                size++;
-            }
-        } catch (NullPointerException e) {
-            e.getMessage();
+        if (size == storage.length) {
+            size--;
+            throw new IndexOutOfBoundsException();
         }
+
+        storage[size] = r;
+        size++;
+
     }
 
-    /**
-     * Gets resume by it's uuid
-     *
-     * @param uuid
-     * @return Resume
-     */
 
     Resume get(String uuid) {
 
@@ -76,40 +60,17 @@ public class ArrayStorage {
 
     void delete(String uuid) {
 
-        //(get(uuid) не возвращает индекс)
+        int index;
 
-        //получается перегруженным циклами, но метод
-        //get(uuid) != null нужен здесь, чтобы temp не удалял 0-й элемент по дефолту
-        //Он нужен, чтобы выяснить, что такое резюме точно существует и можно получить его индекс
-
-        try {
-            if (size != 0 && get(uuid) != null) {
-
-                int temp = 0;
-
-                for (int i = 0; i < size; i++) {
-                    if (uuid.equals(storage[i].uuid)) {
-                        temp = i; //find idx of element
-                    }
-
-                }
-
-                //shift
-
-                //r1 r2 r3 r4 r5
-                //[] [] [] [x] []
-                //r1 r2 r3 r4(5)
-                //[] [] [] []
-
-                for (int j = temp; j < size - 1; j++) {
-                    storage[j] = storage[j + 1];
-                }
-
-                size--;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                index = i;
+                System.arraycopy(storage, index + 1, storage, index, size - index);
             }
-        } catch (IllegalStateException e) {
-            e.getMessage();
         }
+
+        size--;
+
     }
 
     /**

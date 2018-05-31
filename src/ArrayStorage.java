a57caf3db4d9454a3e6c2f32e1defd67698b405d
anyonepaw <32063879+anyonepaw@users.noreply.github.com>
2018-05-31 21:46:21 +0300

    Removed code duplication.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,84 +7,62 @@ import java.util.Arrays;
 
 public class ArrayStorage {
 
-
     private Resume[] storage = new Resume[10000];
     private int size = 0;
 
-    void clear() {
+
+    protected void clear() {
 
         Arrays.fill(storage, 0, size, null);
         size = 0;
 
     }
 
-    void update(Resume r) {
+    protected void update(Resume resume) {
 
-        if (!resumeIsPresent(r.uuid)) {
-            System.out.println("Resume is not present. Cannot update.");
-            return;
-        }
-
-        for (int i = 0; i < size; i++) {
-            if (r.uuid.equals(storage[i].uuid)) {
-                r = new Resume();
-            }
-        }
+        int i = resumeIsPresent(resume.uuid);
+        if (i != -1) storage[i] = new Resume();
 
     }
 
 
-    void save(Resume r) {
+    protected void save(Resume resume) {
 
-        if (resumeIsPresent(r.uuid)) {
+        if (resumeIsPresent(resume.uuid) >= 0) {
             System.out.println("Resume is present in storage already.");
             return;
         }
-
         if (size == storage.length) {
             System.out.println("Storage is full.");
             return;
         }
-
         if (size < storage.length) {
-            storage[size] = r;
+            storage[size] = resume;
             size++;
         }
 
     }
 
 
-    Resume get(String uuid) {
+    protected Resume get(String uuid) {
 
         if (uuid != null) {
-            for (int i = 0; i < size; i++) {
-                if (uuid.equals(storage[i].uuid)) {
-                    return storage[i];
-
-                }
-
-            }
+            int i = resumeIsPresent(uuid);
+            if (i != -1) return storage[i];
         }
         return null;
 
     }
 
 
-    void delete(String uuid) {
+    protected void delete(String uuid) {
 
-        if (!resumeIsPresent(uuid)) {
-            System.out.println("Resume is not present. Cannot delete.");
-            return;
+        int i = resumeIsPresent(uuid);
+        if (i != -1) {
+            storage[i] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
         }
-
-        for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].uuid)) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                break;
-            }
-        }
-        size--;
 
     }
 
@@ -98,20 +76,22 @@ public class ArrayStorage {
 
     }
 
-    int size() {
+    protected int size() {
 
         return size;
 
     }
 
-    private boolean resumeIsPresent(String uuid) {
+
+    private int resumeIsPresent(String uuid) {
 
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
-                return true;
+                return i;
             }
         }
-        return false;
+        System.out.println("Resume is not present.");
+        return -1;
 
     }
 

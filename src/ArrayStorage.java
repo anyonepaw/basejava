8fa7a67af58a0a78092532fb066e8ccaf5bd6497
anyonepaw <32063879+anyonepaw@users.noreply.github.com>
2018-06-01 15:36:56 +0300

    Fix checking of null.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,109 +5,108 @@
 import java.util.Arrays;
 
 
+import static java.util.Objects.*;
+
+
 public class ArrayStorage {
 
-    private Resume[] storage = new Resume[10000];
-    private int size = 0;
+	private Resume[] storage = new Resume[10000];
+	private int size = 0;
 
 
-    protected void clear() {
+	protected void clear() {
 
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
+		Arrays.fill(storage, 0, size, null);
+		size = 0;
 
-    }
+	}
 
-    protected void update(Resume resume) {
+	protected void update(Resume resume) {
 
-        int numberOfResume = findAndCheckIfPresent(resume.uuid);
-        if (numberOfResume >= 0) {
-            storage[numberOfResume] = resume;
-        } else {
-            System.out.println("Resume " + resume.uuid + " is not present.");
-        }
-    }
+		int numberOfResume = findAndCheckIfPresent(resume.uuid);
+		if (numberOfResume >= 0) {
+			storage[numberOfResume] = resume;
+		} else {
+			System.out.println("Resume with uuid = " + resume.uuid + " is not present.");
+		}
 
-
-    protected void save(Resume resume) {
-
-        int numberOfResume = findAndCheckIfPresent(resume.uuid);
-
-        if (numberOfResume >= 0) {
-            System.out.println("Resume is present in storage already.");
-            return;
-        }
-        if (numberOfResume == -2) {
-            System.out.println("You try to save empty resume.");
-            return;
-        }
-        if (size == storage.length) {
-            System.out.println("Storage is full.");
-            return;
-        }
-        if (size < storage.length) {
-            storage[size] = resume;
-            size++;
-        }
-
-    }
+	}
 
 
-    protected Resume get(String uuid) {
+	protected void save(Resume resume) {
 
-        int numberOfResume = findAndCheckIfPresent(uuid);
-        if (numberOfResume >= 0) {
-            return storage[numberOfResume];
-        } else {
-            System.out.println("Resume " + uuid + " is not present.");
-        }
-        return null;
+		requireNonNull(resume.uuid);
+		int numberOfResume = findAndCheckIfPresent(resume.uuid);
+		if (numberOfResume >= 0) {
+			System.out.println("Resume is present in storage already.");
+			return;
+		}
+		if (size == storage.length) {
+			System.out.println("Storage is full.");
+			return;
+		}
+		if (size < storage.length) {
+			storage[size] = resume;
+			size++;
+		}
 
-    }
-
-
-    protected void delete(String uuid) {
-
-        int numberOfResume = findAndCheckIfPresent(uuid);
-        if (numberOfResume >= 0) {
-            size--;
-            storage[numberOfResume] = storage[size];
-            storage[size] = null;
-        } else {
-            System.out.println("Resume " + uuid + " is not present.");
-        }
-    }
+	}
 
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
+	protected Resume get(String uuid) {
 
-        return Arrays.copyOf(storage, size);
+		int numberOfResume = findAndCheckIfPresent(uuid);
+		if (numberOfResume >= 0) {
+			return storage[numberOfResume];
+		}
+		System.out.println("Resume with uuid = " + uuid + " is not present.");
+		return null;
 
-    }
-
-    protected int size() {
-
-        return size;
-
-    }
+	}
 
 
-    private int findAndCheckIfPresent(String uuid) {
+	protected void delete(String uuid) {
 
-        if (uuid != null) {
-            for (int i = 0; i < size; i++) {
-                if (uuid.equals(storage[i].uuid)) {
-                    return i;
-                }
-            }
-            return -1;
-        }
-        return -2;
+		int numberOfResume = findAndCheckIfPresent(uuid);
+		if (numberOfResume >= 0) {
+			size--;
+			storage[numberOfResume] = storage[size];
+			storage[size] = null;
+		} else {
+			System.out.println("Resume with uuid = " + uuid + " is not present.");
+		}
 
-    }
+	}
+
+
+	/**
+	 * @return array, contains only Resumes in storage (without null)
+	 */
+	Resume[] getAll() {
+
+		return Arrays.copyOf(storage, size);
+
+	}
+
+	protected int size() {
+
+		return size;
+
+	}
+
+
+	private int findAndCheckIfPresent(String uuid) {
+
+		if (uuid != null) {
+			for (int i = 0; i < size; i++) {
+				if (uuid.equals(storage[i].uuid)) {
+					return i;
+				}
+			}
+		}
+		return -1;
+
+	}
 
 
 }

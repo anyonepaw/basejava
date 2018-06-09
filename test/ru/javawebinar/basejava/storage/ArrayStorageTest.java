ce995937b16dfaf9dd972b902e538ebc945ff9ce
anyonepaw <32063879+anyonepaw@users.noreply.github.com>
2018-06-09 17:41:33 +0300

    Lesson04 Refactor test methods.

--- test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
@@ -19,28 +19,4 @@ public class ArrayStorageTest extends AbstractArrayStorageTest {
 		super(arrayStorage);
 	}
 
-	@Test
-	public void insertElement() throws Exception {
-		arrayStorage.insertElement(RESUME_FOR_CHECKING, arrayStorage.size);
-		arrayStorage.size++;
-		Resume[] resumes = new Resume[]{new Resume(UUID_1),
-				new Resume(UUID_2), new Resume(UUID_3), RESUME_FOR_CHECKING};
-		Assert.assertArrayEquals(resumes, arrayStorage.getAll());
-	}
-
-	@Test
-	public void fillDeletedElement() throws Exception{
-		int index = arrayStorage.findAndCheckIfPresent("uuid3");
-		arrayStorage.fillDeletedElement(index);
-		arrayStorage.storage[--arrayStorage.size] = null;
-		Resume[] resumes = new Resume[]{new Resume(UUID_1), new Resume(UUID_2)};
-		Assert.assertArrayEquals(resumes, arrayStorage.getAll());
-	}
-
-	@Test
-	public void findAndCheckIfPresent() throws Exception {
-		Assert.assertEquals(0, arrayStorage.findAndCheckIfPresent("uuid1"));
-	}
-
-
 }
--- test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
@@ -1,9 +1,6 @@
 package ru.javawebinar.basejava.storage;
 
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import ru.javawebinar.basejava.model.Resume;
 
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
@@ -22,30 +19,4 @@ public class SortedArrayStorageTest extends AbstractArrayStorageTest {
 		super(sortedArrayStorage);
 	}
 
-	@Test
-	public void insertElement() throws Exception {
-		sortedArrayStorage.insertElement(RESUME_FOR_CHECKING, getIndex(RESUME_FOR_CHECKING.getUuid()));
-		sortedArrayStorage.size++;
-		Resume[] resumes = new Resume[]{new Resume(UUID_1),
-				new Resume(UUID_2), new Resume(UUID_3), RESUME_FOR_CHECKING};
-		Assert.assertArrayEquals(resumes, sortedArrayStorage.getAll());
-	}
-
-	@Test
-	public void fillDeletedElement() throws Exception {
-		sortedArrayStorage.fillDeletedElement(getIndex("uuid3"));
-		sortedArrayStorage.storage[--sortedArrayStorage.size] = null;
-		Resume[] resumes = new Resume[]{new Resume(UUID_1), new Resume(UUID_2)};
-		Assert.assertArrayEquals(resumes, sortedArrayStorage.getAll());
-	}
-
-	@Test
-	public void findAndCheckIfPresent() throws Exception {
-		Assert.assertEquals(0, sortedArrayStorage.findAndCheckIfPresent("uuid1"));
-	}
-
-
-	private int getIndex(String uuid) {
-		return sortedArrayStorage.findAndCheckIfPresent(uuid);
-	}
 }

c19c9ec4f199aa8f27098a173d4a8181f43c3f0d
anyonepaw <32063879+anyonepaw@users.noreply.github.com>
2018-08-19 18:26:52 +0300

    Lesson10 UPD HW10

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -17,12 +17,11 @@ public class DataStreamSerializer implements StreamSerializer {
 
 	@Override
 	public void doWrite(Resume resume, OutputStream os) throws IOException {
-		System.out.println(resume.toString());
 		try (DataOutputStream dos = new DataOutputStream(os)) {
 			dos.writeUTF(resume.getUuid());
 			dos.writeUTF(resume.getFullName());
 			dos.writeInt(resume.getContacts().size());
-			for (Map.Entry<ContactType, String> entry : resume.getContacts().entrySet()){
+			for (Map.Entry<ContactType, String> entry : resume.getContacts().entrySet()) {
 				dos.writeUTF(entry.getKey().name());
 				dos.writeUTF(entry.getValue());
 			}
@@ -37,18 +36,26 @@ public class DataStreamSerializer implements StreamSerializer {
 	public Resume doRead(InputStream is) throws IOException {
 		try (DataInputStream dis = new DataInputStream(is)) {
 			Resume resume = new Resume(dis.readUTF(), dis.readUTF());
-			for (int i = 0; i < dis.readInt(); i++) {
-				resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-			}
-			resume.addSection(SectionType.valueOf(dis.readUTF()), new PlainText(dis.readUTF()));
-			resume.addSection(SectionType.valueOf(dis.readUTF()), new PlainText(dis.readUTF()));
-			resume.addSection(SectionType.valueOf(dis.readUTF()), new StringListText((dis.readUTF()).split("\n")));
-			resume.addSection(SectionType.valueOf(dis.readUTF()), new StringListText((dis.readUTF()).split("\n")));
-			resume.addSection(SectionType.valueOf(dis.readUTF()), organizationTextReader(dis));
-			resume.addSection(SectionType.valueOf(dis.readUTF()), organizationTextReader(dis));
-			return resume;
+			int size = dis.readInt();
+			try {
+				for (int i = 0; i < size; i++) {
+					resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
+				}
+				resume.addSection(SectionType.valueOf(dis.readUTF()), new PlainText(dis.readUTF()));
+				resume.addSection(SectionType.valueOf(dis.readUTF()), new PlainText(dis.readUTF()));
+				resume.addSection(SectionType.valueOf(dis.readUTF()), new StringListText((dis.readUTF()).split("\n")));
+				resume.addSection(SectionType.valueOf(dis.readUTF()), new StringListText((dis.readUTF()).split("\n")));
+				resume.addSection(SectionType.valueOf(dis.readUTF()), organizationTextReader(dis));
+				resume.addSection(SectionType.valueOf(dis.readUTF()), organizationTextReader(dis));
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		return resume;
 	}
+}
+
+
+
 
 	private void textWriter(Text text, DataOutputStream dos) {
 		try {

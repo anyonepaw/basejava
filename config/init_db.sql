0c811f16eedc42e374432a271ef669ad28af7740
anyonepaw <32063879+anyonepaw@users.noreply.github.com>
2018-10-14 10:56:05 +0300

    Lesson15 HW RV1 UPD

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,16 +1,30 @@
 CREATE TABLE resume
 (
   uuid      VARCHAR(36) PRIMARY KEY NOT NULL,
-  full_name VARCHAR          NOT NULL
+  full_name VARCHAR                 NOT NULL
 );
 
 CREATE TABLE contact
 (
-  id          SERIAL   NOT NULL,
+  id          SERIAL      NOT NULL,
   resume_uuid VARCHAR(36) NOT NULL  CONSTRAINT contact_resume_uuid_fk REFERENCES resume ON DELETE CASCADE,
-  type        VARCHAR  NOT NULL,
-  value       VARCHAR  NOT NULL
+  type        VARCHAR     NOT NULL,
+  value       VARCHAR     NOT NULL
 );
 
 CREATE UNIQUE INDEX contact_uuid_type_index
   ON contact (resume_uuid, type);
+
+CREATE TABLE SECTION
+(
+  id          SERIAL      NOT NULL
+    CONSTRAINT section_pkey
+    PRIMARY KEY,
+  resume_uuid VARCHAR(36) NOT NULL  CONSTRAINT section_resume_uuid_fk REFERENCES resume ON DELETE CASCADE,
+  type        VARCHAR     NOT NULL,
+  value       VARCHAR     NOT NULL
+);
+
+CREATE INDEX section_uuid_type_index
+  ON section (resume_uuid, type);
+
